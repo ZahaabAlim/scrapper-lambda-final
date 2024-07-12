fe800b75b82b72a4f06424ac09795a4edd02f0c1
ZahaabAlim <zahaabalim@gmail.com>
2024-07-12 09:45:21 +0530

    aadasd

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "tf-state-bucket020"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "TfStatelock"
+    # dynamodb_table = "TfStatelock"
   }
 }
 
